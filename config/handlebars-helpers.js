d6dc3962e5faca34eaf0c181932b9c1abfd331f7
yun <n856839@gmail.com>
2021-03-16 17:48:34 +0800

    Add GET payment & POST callback

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -4,5 +4,12 @@ module.exports = {
       return options.fn(this)
     }
     return options.inverse(this)
-  },    
+  },
+
+  ifPaid: function(a, b, options) {
+    if (a > b) {
+      return options.fn(this)
+    }
+    return options.inverse(this)
+  }
 }
